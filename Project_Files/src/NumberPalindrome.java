eb67fa2a9fdbacf5db91121a180ac5ef3c88e506
Victoria Rivas <victoria.rivas2711@gmail.com>
2020-10-24 15:25:30 -0400

    Finished challenge

--- Project_Files/src/NumberPalindrome.java
+++ Project_Files/src/NumberPalindrome.java
@@ -1,6 +1,34 @@
 public class NumberPalindrome {
 
-    public static boolean isPalindrome(){
-        
+    /**
+     * Check if the given number is a palindrome
+     * @param number - int
+     * @return boolean
+     */
+    public static boolean isPalindrome(int number){
+        int reverse = 0;
+        int last_digit;
+        int numberCopy = Math.abs(number);
+
+        while(numberCopy != 0){
+            last_digit = numberCopy % 10;
+            reverse += last_digit; // add to most sig. digit
+            numberCopy /= 10; // decrement numberCopy
+            if(numberCopy != 0){
+                reverse *= 10; // increase place
+            }
+        }
+
+        // Check if number is negative
+        if(number < 0){
+            reverse *= -1;
+        }
+
+        // Check if numbers are equal
+        if(reverse == number){
+            return true;
+        } else{
+            return false;
+        }
     }
 }

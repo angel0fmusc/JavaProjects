4277ac8c9d14067d6d6b33694e17fedd4d038cf4
Victoria Rivas <virivas@cisco.com>
2020-10-26 15:52:03 -0400

    Adding exercise 26

---
+++ DiagonalStar/src/DiagonalStar.java
@@ -0,0 +1,18 @@
+public class DiagonalStar {
+
+    /**
+     * Method should print diagonals to generate a rectangular pattern composed of stars (*)
+     * 3Should be accomplished using loops
+     * @param number - int
+     */
+    public static void printSquareStar(int number){
+        if(number < 5){
+            System.out.println("Invalid Value");
+        }
+
+        for(int i = 0; i < number; i++){
+            
+        }
+
+    }
+}

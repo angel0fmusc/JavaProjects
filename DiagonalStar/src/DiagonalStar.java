4fdc24f865da72c89d0fbd61fbf49fcf8feca593
Victoria Rivas <virivas@cisco.com>
2020-10-27 09:42:24 -0400

    Finished printedStar method

--- DiagonalStar/src/DiagonalStar.java
+++ DiagonalStar/src/DiagonalStar.java
@@ -8,10 +8,25 @@ public class DiagonalStar {
     public static void printSquareStar(int number){
         if(number < 5){
             System.out.println("Invalid Value");
+            return;
         }
 
-        for(int i = 0; i < number; i++){
-            
+        for(int row = 1; row <= number; row++){
+            for(int col = 1; col <= number; col++){
+                if(row == 1 || row == number){
+                    System.out.print('*');
+                } else if(col == 1 || col == number){
+                    System.out.print('*');
+                } else if(col == row){
+                    System.out.print('*');
+                } else if(col == (number - row + 1)){
+                    System.out.print('*');
+                } else{
+                    System.out.print(' ');
+                }
+
+            }
+            System.out.println(); // Move to another line
         }
 
     }
--- DiagonalStar/src/Main.java
+++ DiagonalStar/src/Main.java
@@ -1,6 +1,8 @@
+
+
 public class Main {
 
     public static void main(String[] args) {
-
+        DiagonalStar.printSquareStar(2);
     }
 }
